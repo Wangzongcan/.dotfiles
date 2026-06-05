71737b403fc4bc4eed3957f8a1f4a6def0a33f7a
Wangzongcan <wang.zongcan@gmail.com>
2026-06-05 10:42:08 +0800

    hypr: autostart mihomo-party

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -1,6 +1,6 @@
 -- https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function ()
     hl.exec_cmd("fcitx5")
-    hl.exec_cmd("clash-party")
+    hl.exec_cmd("mihomo-party")
     hl.exec_cmd("qs")
 end)
