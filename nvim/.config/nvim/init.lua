9346d2ae163afac40f5fec346882869d3cf6120a
Wangzongcan <wang.zongcan@gmail.com>
2026-06-26 11:01:21 +0800

    Add lualine statusline configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,6 +38,7 @@ vim.pack.add({
   'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/folke/snacks.nvim',
   'https://github.com/MagicDuck/grug-far.nvim',
+  'https://github.com/nvim-lualine/lualine.nvim',
 })
 
 vim.cmd.colorscheme('tokyonight')
@@ -58,6 +59,39 @@ require('mini.surround').setup({
   },
 })
 require('mini.icons').setup()
+require('lualine').setup({
+  options = {
+    theme = 'tokyonight',
+    globalstatus = true,
+    icons_enabled = false,
+    component_separators = { left = '|', right = '|' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = { 'snacks_dashboard' },
+    },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      },
+    },
+    lualine_x = { 'filetype', 'encoding', 'fileformat' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  },
+})
 require('which-key').setup({
   preset = 'helix',
   spec = {
